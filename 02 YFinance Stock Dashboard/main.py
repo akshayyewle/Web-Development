faf5ae23c74c9aadba7813512262efd21e98c6aa
Akshay Yewle <akshayyewle@gmail.com>
2024-04-25 17:14:56 +0100

    Commit

--- 02 YFinance Stock Dashboard/main.py
+++ 02 YFinance Stock Dashboard/main.py
@@ -1,4 +1,5 @@
 from re import S
+from altair import value
 import streamlit as st
 import os
 import pandas as pd
@@ -8,7 +9,7 @@ import stocknews as sn
 from datetime import date
 import plotly.express as px 
 import datetime as dt
-from alpha_vantage.fundamentaldata import FundamentalData
+# from alpha_vantage.fundamentaldata import FundamentalData
 
 # Set API Key
 AV_Key = os.environ.get('Alpha_Vantage_API_Key')
@@ -21,12 +22,11 @@ st.write(f'Today is {Today_Date}')
 
 # Get Input Data
 st.sidebar.subheader(body = f'Get Stock Data')
-Ticker = st.sidebar.text_input(label='Enter Ticker Symbol', placeholder='TSLA', value='TSLA')
+Ticker = st.sidebar.text_input(label='Enter Ticker Symbol', placeholder='MSFT', value='MSFT')
 Start_Date = st.sidebar.date_input(label='Start Date',value = dt.date.today() - dt.timedelta(days=3*365))
 End_Date = st.sidebar.date_input(label='End Date',value=dt.date.today())
 
 # Get Basic Stock Info
-st.subheader(body = f'Basic Info')
 Stock_Data = yf.Ticker(Ticker)
 Stock_Info = Stock_Data.get_info()
 
@@ -44,22 +44,31 @@ st.sidebar.write(f'Industry: {Stock_Industry}')
 st.sidebar.write(f'Sector: {Stock_Sector}')
 st.sidebar.write(f'Currency: {Stock_Description}')
 
-# Get Stock Price Data
+# Create Stock Price Plot
+st.subheader(body = f'Charts')
+st.write('Ticker: ', Ticker,'  |  ','Sector: ',Stock_Sector,' | ','Industry: ',Stock_Industry)
+st.write('Currency: ', Stock_Currency)
+Chart_Type_List = ['Line','Histogram','Area']
+Chart_Type_Input01 = st.selectbox('Select Chart Type', Chart_Type_List)
 Stock_Price_Data = yf.download(tickers=Ticker, start=Start_Date, end=End_Date)
+if Chart_Type_Input01 == 'Area':
+    Stock_Price_Plot = px.area(Stock_Price_Data, x=Stock_Price_Data.index, y='Close')
+    st.plotly_chart(Stock_Price_Plot)
+if Chart_Type_Input01 == 'Line':
+    Stock_Price_Plot = px.line(Stock_Price_Data, x=Stock_Price_Data.index, y='Close')
+    st.plotly_chart(Stock_Price_Plot)
+if Chart_Type_Input01 == 'Histogram':
+    Stock_Price_Plot = px.histogram(Stock_Price_Data, x=Stock_Price_Data.index, y='Close')
+    st.plotly_chart(Stock_Price_Plot)
+
+# Get Stock Price Data
 st.subheader(body = f'Last 10 Trading Session Data')
 st.write('Currency: ', Stock_Currency)
 st.dataframe(Stock_Price_Data.tail(7))
 
-# Create Stock Price Plot
-st.subheader(body = f'Stock Price of {Ticker}')
-st.write('Currency: ', Stock_Currency)
-st.selectbox('Select Chart Type', ['Line','Candlestick','Area'])
-Stock_Price_Plot = px.area(Stock_Price_Data, x=Stock_Price_Data.index, y='Close')
-st.plotly_chart(Stock_Price_Plot)
-
 # Create Tabs
 Tabs_List_01 = ['Pricing Data','Fundamental Data','News','Management','Shareholders','Competitors']
-Pricing_Data, Fundamental_Data, News_Data, Management_Data, Shareholer_Data, \
+Pricing_Data, Fundamental_Data, News_Data, Management_Data, Shareholder_Data, \
     Competitor_Data = st.tabs(tabs=Tabs_List_01)
 
 with Pricing_Data:
@@ -92,8 +101,8 @@ with Pricing_Data:
     st.write('Beta Ratio: ',Stock_Info['beta'])
     st.write('Trailing PE: ',Stock_Info['trailingPE'])
     st.write('Forward PE: ',Stock_Info['forwardPE'])
-    # st.write('Earnings Growth: ',Stock_Info['earningsGrowth']*100)
-    # st.write('Market Capitalization: ',round(Stock_Data.basic_info['marketCap']/(10**9),2),'Billion USD')
+    st.write('Earnings Growth: ',Stock_Info['earningsGrowth']*100)
+    st.write('Market Capitalization: ',round(Stock_Data.basic_info['marketCap']/(10**9),2),'Billion USD')
 
 with Fundamental_Data:
     st.header('Fundamental Data')
@@ -101,8 +110,8 @@ with Fundamental_Data:
     # Balance Sheet
     st.subheader('Balance Sheet')
     Stock_Financial_Sheet = Stock_Data.get_financials()
-    SFS = Stock_Financial_Sheet
-    st.write(SFS)
+    # SFS = Stock_Financial_Sheet
+    st.write(Stock_Financial_Sheet)
     
 with News_Data:
     st.header('News Data')
@@ -110,9 +119,7 @@ with News_Data:
     # Get News Data
     Stock_News = sn.StockNews(Ticker,save_news=False)
     Stock_News_Df = Stock_News.read_rss()
-
-    st.dataframe(Stock_News_Df.head())
-
+    # st.dataframe(Stock_News_Df.head())
     # Display News Data
     for i in range(10):
         st.subheader(Stock_News_Df.iloc[i]['title'])
@@ -137,17 +144,22 @@ with Management_Data:
         else:
             st.write('Salary: ', Officer['totalPay'], Stock_Currency, Officer['fiscalYear'])
         
-with Shareholer_Data:
+with Shareholder_Data:
     st.header('Shareholder Data')
 
     # Major Shareholders
     st.subheader('Major Shareholders')
     Stock_Major_Shareholders = Stock_Data.get_major_holders()
-    Stock_Major_Shareholders.drop('institutionsCount',inplace=True)
-    Stock_Major_Shareholders.sort_values('Value',ascending=False,inplace=True) 
+    # Stock_Major_Shareholders.drop('institutionsCount',inplace=True)
+    # Stock_Major_Shareholders.sort_values('value',ascending=False,inplace=True) 
     # Stock_Major_Shareholders['Value'] = Stock_Major_Shareholders['Value'].apply(lambda x: x*100)     
     st.dataframe(Stock_Major_Shareholders)
 
     # Institutional Investors
     st.subheader('Institutional Investors')
-    st.dataframe(Stock_Data.get_institutional_holders().sort_values('pctHeld',ascending=False))
+    st.dataframe(Stock_Data.get_mutualfund_holders())
+
+with Competitor_Data:
+    st.header('Competitor Data')
+    # Stock_Major_Competitors = Stock_Data.()
+    # st.dataframe(Stock_Major_Competitors)
