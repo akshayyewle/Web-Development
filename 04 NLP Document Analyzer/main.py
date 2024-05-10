f32cf9bec1e2e324a18d4a79da9f1a7c3ef70bde
Akshay Yewle <akshayyewle@gmail.com>
2024-05-10 11:07:20 +0100

    Commit

--- 04 NLP Document Analyzer/main.py
+++ 04 NLP Document Analyzer/main.py
@@ -19,7 +19,6 @@ margins_css = """
         }
     </style>
 """
-
 st.markdown(margins_css, unsafe_allow_html=True)
 
 st.sidebar.image("https://images.unsplash.com/photo-1541854615901-93c354197834?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",width=250)
